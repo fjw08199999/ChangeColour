64c17e0bdd4c263b7a1dd78553b0ea23f6f594b4
FredFu <fjw08199999@gmail.com>
2021-02-14 07:14:17 +0800

    完成畫面 完成畫面與程式碼連結

--- ChangeColour/ViewController.swift
+++ ChangeColour/ViewController.swift
@@ -13,12 +13,55 @@ class ViewController: UIViewController {
     @IBOutlet weak var greenLabel: UILabel!
     @IBOutlet weak var blueLabel: UILabel!
     @IBOutlet weak var alphaLabel: UILabel!
+    @IBOutlet weak var redLableValue: UILabel!
+    @IBOutlet weak var greenLabelValue: UILabel!
+    @IBOutlet weak var blueLabelValue: UILabel!
+    @IBOutlet weak var alphaLabelValue: UILabel!
+    
+    @IBOutlet weak var redSliderValue: UISlider!
+    @IBOutlet weak var greenSliderValue: UISlider!
+    @IBOutlet weak var blueSliderValue: UISlider!
+    @IBOutlet weak var alphaSliderValue: UISlider!
+    
+    @IBOutlet weak var imageView01: UIImageView!
+    @IBOutlet weak var imageView02: UIImageView!
+    
+    @IBOutlet weak var randomButtonText: UIButton!
+    @IBOutlet weak var setButtonText: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        redLabel.text = "紅色"
+        greenLabel.text = "綠色"
+        blueLabel.text = "藍色"
+        alphaLabel.text = "透明度"
+        
     }
 
-
+    @IBAction func redSlider(_ sender: UISlider) {
+        
+    }
+    
+    @IBAction func greenSlider(_ sender: UISlider) {
+        
+    }
+    
+    @IBAction func blueSlider(_ sender: UISlider) {
+        
+    }
+    
+    @IBAction func alphaSlider(_ sender: UISlider) {
+        
+    }
+    
+    @IBAction func randomButton(_ sender: Any) {
+        
+    }
+    
+    @IBAction func setButton(_ sender: Any) {
+        
+    }
+    
 }
 

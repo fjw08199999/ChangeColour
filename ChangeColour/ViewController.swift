594a3f68a7de9bfcd8d933658cf0c109707f5581
FredFu <fjw08199999@gmail.com>
2021-02-14 10:53:58 +0800

    使用function 取代重複的程式碼

--- ChangeColour/ViewController.swift
+++ ChangeColour/ViewController.swift
@@ -30,6 +30,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var randomButtonText: UIButton!
     @IBOutlet weak var setButtonText: UIButton!
     
+    func colorSet() {
+        imageView01.backgroundColor = UIColor(red: CGFloat(redSliderValue.value), green: CGFloat(greenSliderValue.value), blue: CGFloat(blueSliderValue.value), alpha: CGFloat(alphaSliderValue.value))
+    }
+    
+    func colorLabelValueSet() {
+        redLableValue.text = String(format: "%.2f", redSliderValue.value)
+        greenLabelValue.text = String(format: "%.2f", greenSliderValue.value)
+        blueLabelValue.text = String(format: "%.2f", blueSliderValue.value)
+        alphaLabelValue.text = String(format: "%.2f", alphaSliderValue.value)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -37,39 +48,31 @@ class ViewController: UIViewController {
         greenLabel.text = "綠色"
         blueLabel.text = "藍色"
         alphaLabel.text = "透明度"
-        redLableValue.text = String(format: "%.2f", redSliderValue.value)
-        greenLabelValue.text = String(format: "%.2f", greenSliderValue.value)
-        blueLabelValue.text = String(format: "%.2f", blueSliderValue.value)
-        alphaLabelValue.text = String(format: "%.2f", alphaSliderValue.value)
+        
+        colorLabelValueSet()
     }
 
     @IBAction func changeColorSlider(_ sender: UISlider) {
         
         _ = String(format: "%.2f", redSliderValue.value)
-        redLableValue.text = String(format: "%.2f", redSliderValue.value)
         _ = String(format: "%.2f", greenSliderValue.value)
-        greenLabelValue.text = String(format: "%.2f", greenSliderValue.value)
         _ = String(format: "%.2f",blueSliderValue.value)
-        blueLabelValue.text = String(format: "%.2f", blueSliderValue.value)
         _ = String(format: "%.2f", alphaSliderValue.value)
-        alphaLabelValue.text = String(format: "%.2f", alphaSliderValue.value)
         
-        imageView01.backgroundColor = UIColor(red: CGFloat(redSliderValue.value), green: CGFloat(greenSliderValue.value), blue: CGFloat(blueSliderValue.value), alpha: CGFloat(alphaSliderValue.value))
+        colorLabelValueSet()
+        colorSet()
     }
 
     
     @IBAction func randomButton(_ sender: Any) {
         
         redSliderValue.value = Float(CGFloat.random(in: 0...1))
-        redLableValue.text = String(format: "%.2f", redSliderValue.value)
         greenSliderValue.value = Float(CGFloat.random(in: 0...1))
-        greenLabelValue.text = String(format: "%.2f", greenSliderValue.value)
         blueSliderValue.value = Float(CGFloat.random(in: 0...1))
-        blueLabelValue.text = String(format: "%.2f", blueSliderValue.value)
         alphaSliderValue.value = Float(CGFloat.random(in: 0...1))
-        alphaLabelValue.text = String(format: "%.2f", alphaSliderValue.value)
-        
-        imageView01.backgroundColor = UIColor(red: CGFloat(redSliderValue.value), green: CGFloat(greenSliderValue.value), blue: CGFloat(blueSliderValue.value), alpha: CGFloat(alphaSliderValue.value))
+       
+        colorLabelValueSet()
+        colorSet()
     }
     
     @IBAction func setButton(_ sender: Any) {
